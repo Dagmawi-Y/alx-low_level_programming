ac935b0f144ec5c3ec81443d67d306c054ccb584
Dagmawi-Y <dagmawi.ibox@gmail.com>
2024-01-11 03:45:32 -0800

    5 fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,52 +1,34 @@
 #include "main.h"
 
 /**
- * _sqrt_helper - Helper function for binary search.
- * @n: The number to calculate the square root of.
- * @start: The starting point of the search range.
- * @end: The ending point of the search range.
+ * _sqrt_helper - Returns the natural square root of a number.
+ * @n: Input number.
+ * @iterator: Iterator.
  *
- * Return: The square root of the number or -1 if no natural square root.
+ * Return: Square root or -1.
  */
-int _sqrt_helper(int n, int start, int end);
+int _sqrt_helper(int n, int iterator)
+{
+	if (iterator * iterator == n)
+		return (iterator);
+	else if (iterator * iterator > n)
+		return (-1);
+	else
+		return (_sqrt_helper(n, iterator + 1));
+}
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to calculate the square root of.
+ * @n: Input number.
  *
- * Return: The natural square root of the number or -1 if no natural square root.
+ * Return: Natural square root or -1.
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-
 	if (n == 0 || n == 1)
 		return (n);
 
-	return (_sqrt_helper(n, 1, n / 2 + 1));
-}
-
-/**
- * _sqrt_helper - Helper function for binary search.
- * @n: The number to calculate the square root of.
- * @start: The starting point of the search range.
- * @end: The ending point of the search range.
- *
- * Return: The square root of the number or -1 if no natural square root.
- */
-int _sqrt_helper(int n, int start, int end)
-{
-	if (start > end)
-		return (-1);
-
-	int mid = (start + end) / 2;
-	int mid_squared = mid * mid;
-
-	if (mid_squared == n)
-		return (mid);
-	else if (mid_squared < n)
-		return (_sqrt_helper(n, mid + 1, end));
-	else
-		return (_sqrt_helper(n, start, mid - 1));
+	return (_sqrt_helper(n, 1));
 }
