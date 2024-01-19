7881117f8f47f8d3c732df22ef6080c6497c30b8
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-01-19 05:31:37 +0300

    task 1 fix

--- 000exercise/mem_allocation/segf.c
+++ 000exercise/mem_allocation/segf.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
 //
 // Created by Daggy on 1/18/2024.
 //
+/**
+ * segf - Let's segfault \o/
+ *
+ * Return: nothing.
+ */
+void segf(void) {
+    char *str;
+
+    str = "Holberton";
+    str[0] = 's';
+    printf("%s", str);
+}
+
+/**
+ *  main - concept introduction
+ *
+ * Return: 0.
+ */
+int main(void) {
+    segf();
+    return (0);
+}
